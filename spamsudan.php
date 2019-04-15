3c8f0e19f30600e9ec63aa65173441c1cd945ddc
mifkrn <49630744+mifkrn@users.noreply.github.com>
2019-04-15 14:47:13 +0700

    Update spamsudan.php

--- spamsudan.php
+++ spamsudan.php
@@ -52,7 +52,7 @@ class prankcall {
         while ($a < $many) {
             $rand = rand(0123456, 9999999);
             $rands = $this->randStr(12);
-            $post = "method=CALL&countryCode=id&phoneNumber=$no&templateID=pax_android_production";
+            $post = "method=CALL&countryCode=sd&phoneNumber=$no&templateID=pax_android_production";
             $h[] = "x-request-id: ebf61bc3-8092-4924-bf45-$rands";
             $h[] = "Accept-Language: in-ID;q=1.0, en-us;q=0.9, en;q=0.8";
             $h[] = "User-Agent: Grab/5.20.0 (Android 6.0.1; Build $rand)";
